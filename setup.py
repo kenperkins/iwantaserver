1fef88c5b7c5bf13051149959ae09b7667cb728d
briancurtin <brian.curtin@rackspace.com>
2014-10-22 14:34:31 -0500

    Add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name="iwantaserver",
+      version="0.0.0",
+      description="Site to hand out servers like candy",
+      author="Brian Curtin",
+      author_email="brian@python.org",
+      py_modules=["iwantaserver"],
+     )
