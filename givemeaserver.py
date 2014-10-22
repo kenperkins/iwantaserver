cb6e6079aad81d91176d3a7f966d828fd7735059
briancurtin <brian.curtin@rackspace.com>
2014-10-22 11:52:17 -0500

    first cut

---
+++ givemeaserver.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python2.7
+from datetime import datetime
+from ConfigParser import ConfigParser
+import argparse
+import sys
+
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+
+from iwantaserver import db, Event, Server
+
+
+def get_creds(config="libcloud.conf"):
+    parser = ConfigParser()
+    parser.read(config)
+
+    username = parser.get("rackspace", "username")
+    apikey = parser.get("rackspace", "apikey")
+    region = parser.get("rackspace", "region")
+
+    return username, apikey, region
+
+
+def create_servers(size_id, image_id, num, event_id):
+    username, apikey, region = get_creds()
+
+    Driver = get_driver(Provider.RACKSPACE)
+    driver = Driver(username, apikey, region=region)
+
+    size = list(filter(lambda size: size.id == size_id, driver.list_sizes()))[0]
+    image = list(filter(lambda img: img.id == image_id, driver.list_images()))[0]
+
+    for i in range(num):
+        node = driver.create_node(
+            name=datetime.strftime(datetime.now(), "gmas-%Y%m%d%H%M%S"),
+            image=image, size=size)
+        password = node.extra["password"]
+
+        _, ip = driver.wait_until_running([node])[0]
+        server = Server(ip[0], password, event_id)
+        server.available = True
+        db.session.add(server)
+        db.session.commit()
+
+        event = Event.query.filter(Event.id.is_(event_id)).scalar()
+        if not event:
+            continue
+        event.image_name = image.name
+        event.size_name = size.name
+        db.session.add(event)
+        db.session.commit()
+    return i
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--size")
+    parser.add_argument("--image")
+    parser.add_argument("--num", default=1, type=int)
+    parser.add_argument("--event", type=int)
+
+    args = parser.parse_args()
+
+    if args.size is None or args.image is None:
+        print("Missing server arguments")
+        return -1
+    if args.event is None:
+        print("Must provide event ID")
+        return -2
+
+    rv = create_servers(args.size, args.image, args.num, args.event)
+    return rv
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ iwantaserver.py
@@ -0,0 +1,117 @@
+from subprocess import Popen
+import os
+import random
+import sys
+
+from flask import (Flask, request, session, g, redirect, url_for, abort,
+                   render_template, flash)
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/iwantaserver.db"
+app.config["SECRET_KEY"] = "secretlol"
+db = SQLAlchemy(app)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    location = db.Column(db.String)
+    active = db.Column(db.Boolean)
+    image_id = db.Column(db.String, nullable=False)
+    image_name = db.Column(db.String)
+    size_id = db.Column(db.String, nullable=False)
+    size_name = db.Column(db.String)
+    max_servers = db.Column(db.Integer, nullable=False)
+
+    servers = db.relationship("Server", backref="event", lazy="dynamic")
+
+    def __init__(self, name, location, active=False,
+                 image_id="cc6e0096-84f9-4beb-a21e-d80a11a769d8",
+                 size_id="performance1-2", max_servers=5):
+        self.name = name
+        self.location = location
+        self.active = active
+        self.image_id = image_id
+        self.size_id = size_id
+        self.max_servers = max_servers
+
+    def __repr__(self):
+        return '<Event %r %s>' % (self.name, "" if self.active else "(inactive)")
+
+
+class Server(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    available = db.Column(db.Boolean)
+
+    event_id = db.Column(db.Integer, db.ForeignKey("event.id"))
+
+    def __init__(self, ip, password, event_id):
+        self.ip = ip
+        self.password = password
+        self.event_id = event_id
+
+
+@app.route("/event", methods=["GET", "POST"])
+def show_event():
+    event_name = request.args.get("event", None)
+    event = Event.query.filter(Event.name.is_(event_name)).scalar()
+    error = ""
+
+    if not event:
+        return render_template("oops.html",
+            error="Sorry, I've never heard of %s" % event_name)
+
+    # If we're coming in from a post, claim that server and mark it as
+    # no longer available. Then spin up a new one.
+    if request.method == "POST":
+        server = Server.query.filter(
+            Server.event_id.is_(event.id)).filter(
+            Server.ip.is_(request.form["ip"])).filter(
+            Server.available.is_(True)).scalar()
+        if server is None:
+            error = "Oops, someone claimed this one. Here, have another."
+        else:
+            server.available = False
+            server.email = request.form["email"]
+            db.session.add(server)
+            db.session.commit()
+
+            Popen(["../envs/iwant2/bin/python", "givemeaserver.py",
+                   "--image", event.image_id,
+                   "--size", event.size_id, "--num", "1",
+                   "--event", str(event.id)])
+
+    servers = Server.query.filter(
+        Server.event_id.is_(event.id)).filter(
+        Server.available.is_(True)).all()
+    if not servers:
+        error = ("There are no servers available right now. "
+                 "Go check with the Rackspace table and they'll help you out.")
+        return render_template("oops.html", error=error)
+    server = random.choice(servers)
+    return render_template("event.html", error=error, name=event.name,
+                           image=event.image_name, size=event.size_name,
+                           ip=server.ip, password=server.password)
+
+
+@app.route("/", methods=["GET", "POST"])
+def get_event():
+    if request.method == "POST":
+        event = request.form["event"]
+        return redirect(url_for("show_event", event=event))
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "init":
+        if not os.path.exists(os.path.join(os.getcwd(), "data")):
+            os.mkdir("data")
+        db.create_all()
+        print("Initialized database")
+    else:
+        app.run(debug=os.getenv("DEBUG", False))
